c7ae1dcd5bd833aa85d6fab7689b37ff9bb08208
SheikhRashed <devrashed06@gmail.com>
2021-10-25 23:03:06 +0600

    component folder and router added in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import GlobalStyle from './GlobalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import SignUp from './pages/Pricing';
+import Pricing from './pages/Pricing';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/pricing' exact component={Pricing} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
