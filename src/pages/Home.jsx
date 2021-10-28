514aecfec2f91d50b6b7e00ebdea37c4a50e76a8
SheikhRashed <devrashed06@gmail.com>
2021-10-28 22:21:06 +0600

    content section and featured card created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Content } from '../components/Content/Content';
+import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 
+import { heroOne, heroTwo, heroThree } from '../data/HeroData';
 const Home = () => {
   return (
     <>
       <Hero />
+      <Features />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
     </>
   );
 };
